36f9c58cbe9779332a854270ec6babc04f5e4ebf
cyberabis <babishek@gmail.com>
2015-01-06 20:31:21 +0530

    Project init

---
+++ superflow-mvp/superflow/js/superflow.js
@@ -0,0 +1,44 @@
+jsPlumb.ready(function() {
+
+	var jspInstance = jsPlumb.getInstance({
+		Container:"jsplumb-grid",
+		Anchors:[ ["Continuous", {faces:["right"]}], ["Continuous", {faces:["left"]}] ],
+		PaintStyle: { lineWidth : 1, strokeStyle : "DarkGray" },
+		Endpoint: "Blank",
+		//Endpoint : ["Dot", {radius:2}],
+		//EndpointStyle : { fillStyle : "DarkGray" },
+		Connector:["Flowchart", {stub:10}],
+		Overlays:[ ["Arrow", {location:1, width:5, length:5}] ]
+	});
+
+	jspInstance.connect({
+		source:"di-iss-1",
+		target:"di-arg-2-1"
+	});
+
+	jspInstance.connect({
+		source:"di-arg-1-2",
+		target:"di-arg-2-2"
+	});
+
+	jspInstance.connect({
+		source:"di-arg-2-1",
+		target:"di-arg-3-1"
+	});
+
+	jspInstance.connect({
+		source:"di-arg-2-2",
+		target:"di-arg-3-1"
+	});
+
+	jspInstance.connect({
+		source:"di-arg-2-3",
+		target:"di-arg-3-2"
+	});
+
+	jspInstance.connect({
+		source:"di-arg-2-3",
+		target:"di-arg-3-3"
+	});
+
+});
